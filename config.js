08df98ef3670976de3067f8982070d18283080c1
Glitch (anloxbot-gecilcek) <none>
2019-08-28 02:41:41 +0000

    🚣🙅 Checkpoint ./komutlar/ayarlar.js:1774746/7 ./ayarlar.json:1224386/118 ./config.js:1224386/118

--- config.js
+++ config.js
@@ -2,7 +2,7 @@ const config = {
     "ownerID": "446048541488578569", //kendi IDınızı yazınız
     "admins": ["446048541488578569"],
     "support": ["446048541488578569"],
-    "token": "NDg1MTEwODUyNzM5OTIzOTY5.XUHLGg.oG-c1rUkVxAlAweIUxOQfClAixU", //botunuzun tokenini yazınız
+    "token": "NDg1MTEwODUyNzM5OTIzOTY5.XWXoOA.R7fx41HJF9y-JMHWJvxNndZOPzk", //botunuzun tokenini yazınız
     "dashboard" : {
       "oauthSecret": "lNYupwJAzgSjqLSgnf8z5zZITsalw2K4", //botunuzun secretini yazınız
       "callbackURL": `https://anloxbot-gecilcek.glitch.me/callback`, //site URLnizi yazınız /callback kısmını silmeyiniz!
